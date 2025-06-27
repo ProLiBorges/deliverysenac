e82de3c9c4ea6665e2a0923c09c6d0987526333c
ProLiBorges <lilia.bsilveira@senacsp.edu.br>
2025-06-27 11:57:54 -0300

    Adionadas as funcionalidades do carrinho

---
+++ pizzas.js
@@ -0,0 +1,27 @@
+
+let cardapio = [
+    {
+        foto: "imgs/Pizza4.jpg",
+        nome: "Calabresa",
+        ingredientes: ["Calabresa", "Cebola", "Muçarela"],
+        valor: 35.00
+    },
+    {
+        foto: "imgs/Pizza2.jpg",
+        nome: "Muçarela",
+        ingredientes: ["Beringela", "Cebola", "Muçarela"],
+        valor: 40.00
+    },
+    {
+        foto: "imgs/Pizza3.jpg",
+        nome: "Mista de Vegetais",
+        ingredientes: ["Beringela", "Abobrinha", "Pimentão", "Cebola", "Muçarela"],
+        valor: 40.00
+    },
+    {
+        foto: "imgs/FCatupiry.jpg",
+        nome: "Frango com Catupiry",
+        ingredientes: ["Frango", "Catupiry", "Muçarela"],
+        valor: 40.00
+    }
+];
